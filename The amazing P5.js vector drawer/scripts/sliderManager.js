a17ef949f5327453b5bf5a843934ad73c7f21a09
Sveinbjoern <sveinbjoern@gmail.com>
2021-07-02 17:25:27 +0200

    Create sliderManager.js

---
+++ The amazing P5.js vector drawer/scripts/sliderManager.js
@@ -0,0 +1,35 @@
+
+
+// const slidertypes =
+// {
+//     stroke:{
+//         name: "stroke",
+//         currentColor,
+//         strokeWeight,
+//         noStroke,
+
+
+//     },
+
+//     fill:{
+//         name: "fill",
+//     },
+
+//     orderDrawing:{
+//         name: "order of parts",
+//     },
+
+//     orderAnimate:{
+//         name: "order of figure"
+//     },
+
+//     orderCanvas:{
+//         name: "order of figure"
+//     },
+// }
+
+// const sliderConsts = 
+// {
+//     x = 300,
+//     y = 100,
+// }
