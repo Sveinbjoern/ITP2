dffee013d3bf0c849d9a72676572c31484a952a5
Sveinbjoern <sveinbjoern@gmail.com>
2021-07-02 17:26:22 +0200

    Update slide.js

---
+++ The amazing P5.js vector drawer/scripts/slide.js
@@ -0,0 +1,16 @@
+// Every configuration and option will be in a slide
+//by default the slides are in the sidebarLeft or slidebarRight
+
+function Slide (location, position, type){
+
+    let x = location[0];
+    let y = location[1];
+
+    // slide should be in the bar location
+
+    // slide should be the position element of that location
+
+    // slide should be of the type type
+
+}
+
