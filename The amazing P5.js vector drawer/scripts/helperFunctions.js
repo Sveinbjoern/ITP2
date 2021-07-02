e7e72d0731e0e4af053dc2c79d20bafbe437f969
Sveinbjoern <sveinbjoern@gmail.com>
2021-07-02 17:27:41 +0200

    Update helperFunctions.js

--- The amazing P5.js vector drawer/scripts/helperFunctions.js
+++ The amazing P5.js vector drawer/scripts/helperFunctions.js
@@ -6,8 +6,9 @@ function HelperFunctions() {
 
 	//event handler for the clear button event. Clears the screen
 	select("#clearButton").mouseClicked(function() {
-		background (255);
-
+		
+        drawManager.figures[0] = new Figure("start");
+        drawManager.reset();
 		//call loadPixels to update the drawing state
 		//this is needed for the mirror tool
 		loadPixels();
