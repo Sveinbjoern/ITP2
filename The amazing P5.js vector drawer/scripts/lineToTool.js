4957e8f24d978c83c205dc3d55d979769327cb60
Sveinbjoern <sveinbjoern@gmail.com>
2021-07-02 17:27:47 +0200

    Update lineToTool.js

--- The amazing P5.js vector drawer/scripts/lineToTool.js
+++ The amazing P5.js vector drawer/scripts/lineToTool.js
@@ -3,42 +3,82 @@
 	Hold the mouse at the start of the line, press, hold and drag to where you want it to end
 */
 
-function LineToTool(){
-	this.icon = "assets/lineTo.jpg";
-	this.name = "LineTo";
+function LineToTool() {
+  this.icon = "assets/lineTo.jpg";
+  this.name = "LineTo";
+	self =this;
+  this.lightMode = false;
+  //drawing turns true when we press the buttons and you will have a display of the line
+  let drawn = false;
+  this.arrayLength = 0 ;
+  this.currentVertex = [];
 
-	//startMouse varibles for the starting point of the line. 
-	var startMouseX;
-	var startMouseY;
-	
-	//drawing turns true when we press the buttons and you will have a display of the line
-	var drawing = false;
-	
-	//draws a line when mouse is pressed, keeps the one that is there when you release
-	this.draw = function(){
-		//check if mouse is pressed
-		if(mouseIsPressed){
-			// If you haven't started drawing the start drawing and set startMouse to the current mouse values
-			if(!drawing){
-				loadPixels(); // loadPixels is in this case used to save the screeen at this point
-				startMouseX = mouseX;
-				startMouseY = mouseY;
-				drawing = true;
-				
-			}
+  //draws a line when mouse is pressed, keeps the one that is there when you release
+  this.draw = function () {
+    this.arrayLength =
+      drawManager.figures[currentFigure].drawings[currentDrawing].parts[currentPart].vertexArray.length;
+    this.currentVertex =
+      drawManager.figures[currentFigure].drawings[currentDrawing].parts[
+        currentPart
+      ].vertexArray;
 
-			else{
-				updatePixels(); // updatePixels recovers the screen from the load, removing lines you do not want to draw
-				line(startMouseX, startMouseY, mouseX, mouseY); //draws the line 
-			}
-
-		}
-		// if the mouse is not pressed nothing new is drawn but if a line was made in the last frame it is kept
-		else if(drawing){
-			drawing = false; //reseting the program to be able to draw again with a new startMouse
+    if (!drawn) {
 		
+			loadPixels(); // loadPixels is in this case used to save the screeen at this point
+		   
+      drawn = true;
+    }
+    if (arrayLength > 0 && mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+		if (this.lightMode)
+		{
+				updatePixels();
+				line(	this.currentVertex[arrayLength-1][0],
+						this.currentVertex[arrayLength-1][1],
+						mouseX,mouseY);
+		} else
+		{
+			this.currentVertex.push ([mouseX,mouseY])
+			drawManager.reset();
+			this.currentVertex.pop();
 		}
-	};
+		
+    } else {updatePixels()}
 
+    
+  };
+
+  this.setup = function(){
+	
+		mousePressed = function () {
+			console.log("mousePressed in lineToTool")
+		  //make mouse only work inside canvas
+		  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+			self.currentVertex.push([mouseX, mouseY]);
+			drawManager.reset();
+			self.drawn = false;
+		  }
+		  // prevent default
+		  return false;
+		};
+  
+		mouseDragged = function () {
+		  //empty in this drawingMode
+  
+		  // prevent default
+		  return false;
+		};
+		mouseMoved = function () {
+		  //draws the line
+  
+		  // prevent default
+		  return false;
+		};
+		mouseReleased = function () {
+		  //empty in this drawingMode
+		  // prevent default
+		  return false;
+		};
+	  
+  } 
 
 }
